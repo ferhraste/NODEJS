7b35658cb0798b3c454940f34e4443b51d31aca4
Fernando Hraste <fernando.hraste@bibamgroup.com>
2021-09-05 20:30:54 -0300

    resguardo files

--- ej_webserver.js
+++ ej_webserver.js
@@ -1,7 +1,13 @@
 var http = require("http");
-var entorno = require('./modulos/config.js').config()
+let fs = require('fs');
+var entorno = require('./modulos/config.js').config();
+var datosJS = fs.readFile('files/datos.json', 'utf-8', function (err, data) {
+    if(err) {
+      console.log('error: ', err);
+    }
+});
 var server = http.createServer(function (peticion, respuesta){
-   respuesta.end("Hola Loco Mundo ");
+   respuesta.end(JSON.stringify("pepe"));
 });
 
 var puerto;
