f62a308eb19eb00bc9d95f11f9a1f160e95b4537
Fernando Hraste <fernando.hraste@bibamgroup.com>
2021-09-05 20:40:36 -0300

    final ej 1 web server

--- ej_webserver.js
+++ ej_webserver.js
@@ -4,18 +4,20 @@ var entorno = require('./modulos/config.js').config();
 var datosJS = fs.readFile('files/datos.json', 'utf-8', function (err, data) {
     if(err) {
       console.log('error: ', err);
+    } else {
+        var server = http.createServer(function (req,res){
+            res.setHeader('Content-Type', 'application/json');
+            res.end(data);
+        });
+        
+        var puerto;
+        if(entorno.PORT) {
+          puerto = entorno.PORT;
+        } else {
+          puerto = 3000;
+        }
+        server.listen(puerto, function(){
+           console.log("tu server en " + this.address().port);
+        });
     }
-});
-var server = http.createServer(function (peticion, respuesta){
-   respuesta.end(JSON.stringify("pepe"));
-});
-
-var puerto;
-if(entorno.PORT) {
-  puerto = entorno.PORT;
-} else {
-  puerto = 3000;
-}
-server.listen(puerto, function(){
-   console.log("tu server en " + this.address().port);
 });
